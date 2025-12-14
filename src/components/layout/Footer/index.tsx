efebe384fc84167d70145a760666b32084be2771
sarbagya <sarbagyaghoshrestha@gmail.com>
2025-12-14 11:43:56 +0545

    bug: fix the footer irregularity aka placement of location in the footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -152,10 +152,10 @@ export function Footer() {
                       e.currentTarget.style.backgroundColor = 'rgba(229, 121, 14, 0.1)';
                     }}>
                       <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                       </svg>
                     </div>
-                    <span className="text-base font-medium mt-2">{siteConfig.contact.email}</span>
+                    <span className="text-base font-medium mt-2">{siteConfig.contact.phone}</span>
                   </a>
                 </li>
                 <li>
@@ -178,10 +178,8 @@ export function Footer() {
                   </a> 
                 </li>
                 <li>
-                  <a
-                    href={`tel:${siteConfig.contact.phone}`}
-                    className="group flex items-start gap-4 text-gray-600 hover:text-[#e5790e] transition-colors"
-                  >
+                  <div  className="group flex items-start gap-4 text-gray-600 hover:text-[#e5790e] transition-colors">
+
                     <div className="flex items-center justify-center w-10 h-10 rounded-xl flex-shrink-0 group-hover:text-white transition-colors" style={{ backgroundColor: 'rgba(0, 165, 79, 0.1)', color: '#00a54f' }}
                     onMouseEnter={(e) => {
                       e.currentTarget.style.backgroundColor = '#00a54f';
@@ -195,7 +193,8 @@ export function Footer() {
                       </svg>
                     </div>
                     <span className="text-base font-medium mt-2">{siteConfig.contact.address}</span>
-                  </a>
+
+                  </div>
                 </li>
                 <li className="pt-2">
                   <a
